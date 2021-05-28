0614c16d1ce937c9241ef09b779de5a8eec43f09
Shauli Kraus <shaulikr07@gmail.com>
2021-05-28 18:38:33 +0300

    Update DataQueryServiceImpl

--- src/main/java/service/DataQueryServiceImpl.java
+++ src/main/java/service/DataQueryServiceImpl.java
@@ -6,42 +6,87 @@ import api.Item;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import api.QueryParseException;
+
 public class DataQueryServiceImpl implements DataQueryService {
     Map<String, Item> items = new HashMap<String, Item>();
-    public List<Item> query(String query){
+    public List<Item> query(String query) throws Exception{
+        if(query.equals(""))
+            return new ArrayList<Item>(items.values());
+        Pattern pattern = Pattern.compile("^(EQUAL|GREATER_THAN|LESS_THAN)\\((.*?)\\,(.*)\\)");
+        Matcher matcher = pattern.matcher(query);
+        if(matcher.find()){
+            String property = matcher.group(2);
+            String value = matcher.group(3);
 
-        if (query.startsWith("EQUAL")){
-            Equal();
+            if(matcher.group(1).equals("EQUAL"))
+                return Equal(property, value);
+            else if(matcher.group(1).equals("GREATER_THAN"))
+                return GreaterThan(property, value);
+            else if(matcher.group(1).equals("LESS_THAN"))
+                return LessThan(property, value);
+            else
+                throw new QueryParseException("Invalid query", null);
         }
-        if (query.startsWith("GREATER_THAN")) {
-            GreaterThan();
+        Pattern aoPattern = Pattern.compile("^(AND|OR)\\((.*)\\)");
+        Matcher aoMatcher = aoPattern.matcher(query);
+        if(aoMatcher.find()){
+            String functionArgs = aoMatcher.group(2);
+            Pattern firstBracketP = Pattern.compile("^.*?\\(");
+            Matcher firstBracketM = firstBracketP.matcher(functionArgs);
+            if(firstBracketM.find()){
+                int pos = firstBracketM.end();
+                int count = 1;
+                while(pos < functionArgs.length() && count > 0){
+                    if(functionArgs.charAt(pos) == '(')
+                        count++;
+                    else if(functionArgs.charAt(pos) == ')')
+                        count--;
+                    pos++;
+                }
+                if(count == 0) {
+                    String arg1 = functionArgs.substring(0, pos);
+                    String arg2 = functionArgs.substring(pos + 1, functionArgs.length());
+
+                    if(aoMatcher.group(1).equals("AND")){
+                        return Intersection(arg1,arg2);
+                    }
+                    else if(aoMatcher.group(1).equals("OR")){
+                        return Union(arg1,arg2);
+                    }
+                    else
+                        throw new QueryParseException("Invalid query", null);
+                }
+                else
+                    throw new QueryParseException("Invalid query", null);
+            }
+            else
+                throw new QueryParseException("Invalid query", null);
         }
-        if (query.startsWith("LESS_THAN")) {
-            LessThan();
+        Pattern notPattern = Pattern.compile("^NOT\\((.*?)\\)");
+        Matcher notMatcher = notPattern.matcher(query);
+        if(notMatcher.find()){
+            return Complement(notMatcher.group(1));
         }
-        if (query.startsWith("AND")) {
-            Intersection();
-        }
-        if (query.startsWith("OR")) {
-            Union();
-        }
-        if (query.startsWith("NOT")) {
-            Compliment();
-        }
+        throw new QueryParseException("Invalid query", null);
+
     }
 
     public List<Item> Equal(String property, String value) {
         List<Item> response = new ArrayList<Item>();
-        if(property.startsWith("id")){
+        if(property.equals("id")){
             String searchId = value.replaceAll("\"","");//Remove quote from string
             if (this.items.get(searchId) != null)//Check if the search id is not null
                 response.add(this.items.get(searchId));
             else {
                 System.out.print("No item has been found");
-                return null;
+                return new ArrayList<Item>();
             }
         }
-        if(property.startsWith("title")){
+        if(property.equals("title")){
             String searchTitle = value.replaceAll("\"","");//Remove quote from string
             if(items.containsValue(searchTitle)){
                 for(Map.Entry<String, Item> entry : items.entrySet()){
@@ -52,10 +97,11 @@ public class DataQueryServiceImpl implements DataQueryService {
             }
             else {
                 System.out.print("No item has been found");
-                return null;
+                return new ArrayList<Item>();
+
             }
         }
-        if(property.startsWith("content")){
+        if(property.equals("content")){
             String searchContents = value.replaceAll("\"","");//Remove quote from string
             if(items.containsValue(searchContents)){
                 for(Map.Entry<String, Item> entry : items.entrySet()){
@@ -66,11 +112,12 @@ public class DataQueryServiceImpl implements DataQueryService {
             }
             else {
                 System.out.print("No item has been found");
-                return null;
+                return new ArrayList<Item>();
+
             }
         }
-        if(property.startsWith("views")){
-            int searchViews = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
+        if(property.equals("views")){
+            int searchViews = Integer.parseInt(value);//Remove quote from string
             if(items.containsValue(searchViews)){
                 for(Map.Entry<String, Item> entry : items.entrySet()){
                     if(entry.getValue().getViews() == searchViews){
@@ -80,11 +127,12 @@ public class DataQueryServiceImpl implements DataQueryService {
             }
             else {
                 System.out.print("No item has been found");
-                return null;
+                return new ArrayList<Item>();
+
             }
         }
-        if(property.startsWith("timestamp")){
-            int searchTimestamps = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
+        if(property.equals("timestamp")){
+            int searchTimestamps = Integer.parseInt(value);//Remove quote from string
             if(items.containsValue(searchTimestamps)){
                 for(Map.Entry<String, Item> entry : items.entrySet()){
                     if(entry.getValue().getTimestamp() == searchTimestamps){
@@ -94,17 +142,25 @@ public class DataQueryServiceImpl implements DataQueryService {
             }
             else {
                 System.out.print("No item has been found");
-                return null;
+                return new ArrayList<Item>();
             }
         }
         return response;
     }
     public List<Item> GreaterThan(String property, String value){
         List<Item> response = new ArrayList<Item>();
-        if(property.startsWith("views")){
+        if(property.equals("views")){
             int searchViews = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
             for(Map.Entry<String, Item> entry : items.entrySet()){
-                if(entry.getValue().getViews() < searchViews){
+                if(entry.getValue().getViews() > searchViews){
+                    response.add(entry.getValue());
+                }
+            }
+        }
+        else if(property.equals("timestamp")){
+            int searchTimeStamp = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
+            for(Map.Entry<String, Item> entry : items.entrySet()){
+                if(entry.getValue().getTimestamp() > searchTimeStamp){
                     response.add(entry.getValue());
                 }
             }
@@ -117,10 +173,18 @@ public class DataQueryServiceImpl implements DataQueryService {
     }
     public List<Item> LessThan(String property, String value){
         List<Item> response = new ArrayList<Item>();
-        if(property.startsWith("views")){
+        if(property.equals("views")){
             int searchViews = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
             for(Map.Entry<String, Item> entry : items.entrySet()){
                 if(entry.getValue().getViews() < searchViews){
+                    response.add(entry.getValue());
+                }
+            }
+        }
+        else if(property.equals("timestamp")){
+            int searchTimeStamp = Integer.parseInt(value.replaceAll("\"",""));//Remove quote from string
+            for(Map.Entry<String, Item> entry : items.entrySet()){
+                if(entry.getValue().getTimestamp() < searchTimeStamp){
                     response.add(entry.getValue());
                 }
             }
@@ -132,19 +196,44 @@ public class DataQueryServiceImpl implements DataQueryService {
         return response;
 
     }
-    public List<Item> Intersection(List<Item> a,List<Item> b){
-        return a.stream().filter(b::contains).collect(Collectors.toList());
-    }
-    public List<Item> Union(List<Item> a,List<Item> b){
-        Set<Item> s1 = new HashSet<>(a);
-        s1.addAll(b);
-        return new ArrayList<>(s1);
-    }
-    public List<Item> Compliment(List<Item> a){
+    public List<Item> Intersection(String a,String b) throws Exception{
+
+        try {
+            List<Item> listA = query(a);
+            List<Item> listB = query(b);
+            return listA.stream().filter(listB::contains).collect(Collectors.toList());
+        }
+        catch(Exception e) {
+            throw new QueryParseException("Invalid query", null);
+        }
+
 
     }
+    public List<Item> Union(String a,String b) throws Exception{
+        try {
+            List<Item> listA = query(a);
+            List<Item> listB = query(b);
+            Set<Item> s1 = new HashSet<>(listA);
+            s1.addAll(listB);
+            return new ArrayList<>(s1);
+        }
+        catch(Exception e) {
+            throw new QueryParseException("Invalid query", null);
+        }
+    }
+    public List<Item> Complement(String a)throws Exception{
+        try {
+            List<Item> listA = query(a);
+            List<Item> itemValue = new ArrayList<Item>(items.values());
+            itemValue.removeAll(listA);
+            return itemValue;
+        }
+        catch(Exception e) {
+            throw new QueryParseException("Invalid query", null);
+        }
+    }
 
-    public void save(Item item){
+    public void save(Item item) throws Exception{
         items.put(item.getId(), item);
     }
 }
